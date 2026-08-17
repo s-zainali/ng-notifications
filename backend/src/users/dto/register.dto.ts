89fe8cee7fe26bd627f730e7f9f4a830454eaec7
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 00:22:58 +0500

    Created user register DTO

--- backend/src/users/dto/register.dto.ts
+++ backend/src/users/dto/register.dto.ts
@@ -1,4 +1,17 @@
+import { IsNotEmpty, IsString, MinLength, NotContains } from "class-validator";
 
 export class RegisterDto {
-    
+    @IsString()
+    @IsNotEmpty({message: 'Full name is required'})
+    fullName: String;
+
+    @IsString()
+    @IsNotEmpty({message: 'Username is required'})
+    @NotContains(' ', {message: 'Username cannot contain spaces'})
+    username:String;
+
+    @IsString()
+    @MinLength(6, {message: 'Password must be at least 6 characters'})
+    password:String;
+
 }
