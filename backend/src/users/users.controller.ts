025f1d40bb69c2480b87bb559a5f1289b1394ac0
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 01:31:01 +0500

    implement user controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "./dto/login.dto";
 
-@Controller()
+@Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @Post('register')
+    async register(@Body() registerDto: RegisterDto) {
+        this.usersService.register(registerDto)
+    }
 
+    @Post('login')
+    async login(@Body() loginDto: LoginDto) {
+        this.usersService.login(loginDto)
+    }
 }
