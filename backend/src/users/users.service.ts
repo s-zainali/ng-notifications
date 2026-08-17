bf11fabe22f31c7afb3dae32f4d3e483b9d266d5
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 01:08:53 +0500

    created register and login functions in user service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { RegisterDto } from "./dto/register.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User, UserDocument } from "./user.schema";
+import { JwtService } from "@nestjs/jwt";
+import { LoginDto } from "./dto/login.dto";
 
 @Injectable()
 export class UsersService {
-    
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+        private jwtService: JwtService
+    ){}
+
+    async register(registerDto: RegisterDto): Promise<{message: string}> {
+        const {fullName, username, password} = registerDto;
+
+        return ;
+    }
+
+    async login(loginDto: LoginDto) : Promise<{message:string}> {
+        const {username, password} = loginDto;
+
+        return;
+    }
 }
