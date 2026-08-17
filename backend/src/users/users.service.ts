9106da162edcd9c9a2248f6a54d5c8fe3e6570b8
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 01:18:37 +0500

    implemented register service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,36 +1,44 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { RegisterDto } from "./dto/register.dto";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { User, UserDocument } from "./user.schema";
-import { JwtService } from "@nestjs/jwt";
-import { LoginDto } from "./dto/login.dto";
-import * as bcrypt from 'bcrypt'
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel(User.name) private userModel: Model<UserDocument>,
-        private jwtService: JwtService
-    ){}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
-    async register(registerDto: RegisterDto): Promise<{message: string}> {
-        const {fullName, username, password} = registerDto;
+  async register(registerDto: RegisterDto): Promise<{ message: string }> {
+    const { fullName, username, password } = registerDto;
 
-        const existingUser = await this.userModel.findOne({username})
-        if (existingUser) {
-            throw new BadRequestException('Username is already taken')
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const passwordHash = await bcrypt.hash(password, salt)
-
-        return ;
+    const existingUser = await this.userModel.findOne({ username });
+    if (existingUser) {
+      throw new BadRequestException('Username is already taken');
     }
 
-    async login(loginDto: LoginDto) : Promise<{message:string}> {
-        const {username, password} = loginDto;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        return;
-    }
+    const newUser = new this.userModel({
+      fullName,
+      username,
+      password: passwordHash,
+    });
+
+    await newUser.save()
+
+    return {message: "New user created successfully"};
+  }
+
+  async login(loginDto: LoginDto): Promise<{ message: string }> {
+    const { username, password } = loginDto;
+
+    return;
+  }
 }
