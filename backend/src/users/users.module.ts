cf5d2c1a97f99db73c5db4409b605f8d84f945ca
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 01:41:59 +0500

    implement Users Module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,24 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./user.schema";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+        JwtModule.registerAsync({
+            global: true,
+            imports: [ConfigModule],
+            inject: [ConfigModule],
+            useFactory: async (configService : ConfigService) => ({
+                secret : configService.get<string>('JWT_SECRET'),
+                signOptions: {expiresIn: '1hr'}
+            }) 
+        })
+    ],
     controllers : [UsersController],
     providers : [UsersService],
 })
