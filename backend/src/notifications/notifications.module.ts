cf192f6e45f2161b1ab6945ce1366f350fa0627e
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 18:51:39 +0500

    implements notifications module

---
+++ backend/src/notifications/notifications.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { NotificationsController } from "./notifications.controller";
+import { NotificationsService } from "./notifications.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { NotificationSchema } from "./notification.schema";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }])
+    ],
+    controllers: [NotificationsController],
+    providers: [NotificationsService],
+    exports: [NotificationsService]
+})
+
+export class NotificationsModule {}
