7d30bc5c9e88fa5e766011eb4b498dd3c10a0526
Syed Zain Ali <szainali284@gmail.com>
2026-08-19 01:36:13 +0500

    Populated notification card buttons

--- frontend/src/components/NotificationCard.tsx
+++ frontend/src/components/NotificationCard.tsx
@@ -1,10 +1,36 @@
-function NotificationCard({category, header, body}) {
-    return (
-        <div className={`flex flex-col justify-start p-2 border-1 rounded-xl`}>
-            <h2>{header}</h2>
-            <p>{body}</p>
+function NotificationCard({ category, header, body, isLive }) {
+  return (
+    <div
+      className={`flex justify-between p-2 border-2 rounded-xl 
+        ${
+          category === "INFO"
+            ? "border-blue-600 bg-blue-500/70 text-black-70"
+            : category === "WARNING"
+            ? "border-amber-400 bg-amber-300/70 text-black-70"
+            : category === "ERROR"
+            ? "border-red-600 bg-red-500/70 text-black-70"
+            : ""
+        }`}
+    >
+      <div className="flex flex-col">
+        <h2 className="text-sm uppercase font-bold">{header}</h2>
+        <p className="text-sm">{body}</p>
+      </div>
+      {!isLive && (
+        <div className="flex items-center justify-between gap-4 opacity-70">
+          <button className="cursor-pointer" title="Notify">
+            <img src="/bell.png" alt="Notify" className="h-5" />
+          </button>
+          <button className="cursor-pointer" title="Edit">
+            <img src="/edit.png" alt="Edit" className="h-5" />
+          </button>
+          <button className="cursor-pointer" title="Delete"> 
+            <img src="/delete.png" alt="delete" className="h-5" />
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default NotificationCard
+export default NotificationCard;
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -6,18 +6,18 @@ function DashboardPage() {
       <header className="grid grid-cols-3 bg-alabaster-grey-100 p-4 rounded-3xl border-1 border-ink-black-800">
         <div className="flex items-center justify-start ">
           <div className="flex gap-4 justify-start items-center rounded-xl border-1 border-ink-black-700 bg-ink-black-800 px-2 h-full">
-            <img src="/sdf" alt="🧔️" />
+            <img src="/userAvatar.png" className="h-5" alt="🧔️" />
             <span className="text-sm font-bold text-alabaster-grey-50">
               Full Name{" "}
             </span>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center">
-            <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
+          <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
           <h1 className="text-center text-2xl text-ink-black-800 font-bold tracking-widest">
             NG-Notifications
           </h1>
-            <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
+          <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
         </div>
         <div className="flex justify-end items-center h-full ">
           <button className="border border-ink-black-700 bg-ink-black-800 text-alabaster-grey-50 uppercase font-bold cursor-pointer rounded-xl h-full px-5 text-xs ">
@@ -28,24 +28,35 @@ function DashboardPage() {
       <main className="flex flex-col gap-6 h-full">
         <div className="bg-alabaster-grey-100 w-full h-50 rounded-3xl border-1 borer-ink-black-800 p-4"></div>
         <div className="bg-alabaster-grey-100 w-full h-full rounded-3xl border-1 borer-ink-black-800 p-4 flex flex-col gap-4">
-            <h2 className="text-xl font-bold text-ink-black-800">My Notifications</h2>
-            <div className="flex flex-col gap-4">
-                <NotificationCard 
-                    category={'INFO'}
-                    header={'Sample Info Header'}
-                    body={'This is a sample body for info'}
-                    />
-                <NotificationCard 
-                    category={'WARNING'}
-                    header={'Sample Warning Header'}
-                    body={'This is a sample body for warning'}
-                    />
-                <NotificationCard 
-                    category={'ERROR'}
-                    header={'Sample Error Header'}
-                    body={'This is a sample body for error'}
-                    />
-            </div>
+          <div className="flex justify-between items-start">
+            <h2 className="text-xl font-bold text-ink-black-800">
+              My Notifications
+            </h2>
+            <button className="cursor-pointer flex gap-1 items-center">
+                <span className="text-sm font-bold tracking-tight text-black/70">ADD</span>
+                <img src="/addition.png" alt="Add" className="w-7 opacity-70" />
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <NotificationCard
+              category={"INFO"}
+              header={"Sample Info Header"}
+              body={"This is a sample body for info"}
+              isLive={false}
+            />
+            <NotificationCard
+              category={"WARNING"}
+              header={"Sample Warning Header"}
+              body={"This is a sample body for warning"}
+              isLive={false}
+            />
+            <NotificationCard
+              category={"ERROR"}
+              header={"Sample Error Header"}
+              body={"This is a sample body for error"}
+              isLive={false}
+            />
+          </div>
         </div>
       </main>
     </div>
