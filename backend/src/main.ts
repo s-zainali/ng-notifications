e2fa4da75b2ac362fec30a235040e7dbc88d0033
Syed Zain Ali <szainali284@gmail.com>
2026-08-19 01:41:52 +0500

    enabled Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: 'http"//127.0.0.1:5173'})
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 3000);
 }
