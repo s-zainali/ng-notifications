a02da1852ae3f1b41875808cb1227f46702ba5bb
Syed Zain Ali <szainali284@gmail.com>
2026-08-19 00:50:37 +0500

    styled header

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,17 +3,22 @@ function DashboardPage() {
     <div className="h-[100dvh] w-[100dvw] bg-alabaster-grey-200 flex flex-col gap-6 p-6">
       <header className="grid grid-cols-3 bg-alabaster-grey-100 p-4 rounded-3xl border-1 border-ink-black-800">
         <div className="flex items-center justify-start ">
-          <div className="flex gap-4 justify-start items-center rounded-xl border-1 border-ink-black-700 bg-ink-black-800 py-1 px-2">
+          <div className="flex gap-4 justify-start items-center rounded-xl border-1 border-ink-black-700 bg-ink-black-800 px-2 h-full">
             <img src="/sdf" alt="🧔️" />
-            <span className="text-sm font-bold text-alabaster-grey-50">Full Name </span> 
-            <span className="text-xs text-alabaster-grey-50">@username</span>
+            <span className="text-sm font-bold text-alabaster-grey-50">
+              Full Name{" "}
+            </span>
           </div>
         </div>
-        <h1 className="text-center text-2xl text-ink-black-800 font-bold tracking-widest">
-          NG-Notifications
-        </h1>
+        <div className="flex flex-col justify-center items-center">
+            <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
+          <h1 className="text-center text-2xl text-ink-black-800 font-bold tracking-widest">
+            NG-Notifications
+          </h1>
+            <div className="w-xl h-[1px] bg-gradient-to-r from-transparent via-ink-black-800 to-transparent "></div>
+        </div>
         <div className="flex justify-end items-center h-full ">
-          <button className="border border-ink-black-700 bg-ink-black-800 text-alabaster-grey-50 uppercase font-bold cursor-pointer rounded-xl py-2 px-5 text-xs ">
+          <button className="border border-ink-black-700 bg-ink-black-800 text-alabaster-grey-50 uppercase font-bold cursor-pointer rounded-xl h-full px-5 text-xs ">
             Log Out
           </button>
         </div>
