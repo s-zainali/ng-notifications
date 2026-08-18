f3f5699895fc013370815eead056fd10ca394a31
Syed Zain Ali <szainali284@gmail.com>
2026-08-18 20:11:11 +0500

    implemented auth service frontend

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,35 @@
+import { api } from "./api";
+
+export interface LoginCredentials {
+    email: string;
+    password: string;
+}
+
+export interface RegisterCredentials extends LoginCredentials{
+    fullName: string;
+}
+
+export interface AuthResponse {
+    auth_token: string;
+    user: {
+        id: string;
+        fullname: string;
+        email: string;
+    }
+}
+
+export const authService = {
+    async login(credentials: LoginCredentials): Promise<AuthResponse>{
+        const response = await api.post('/users/login', credentials);
+        return response.data;
+    },
+
+    async register(credentials: RegisterCredentials): Promise<{message: string}> {
+        const response = await api.post('/users/register', credentials);
+        return response.data;
+    },
+
+    logout(): void {
+        localStorage.removeItem('token')
+    }
+}
